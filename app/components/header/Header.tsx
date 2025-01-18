3a7522e50782fa49a12398e9285f12b4481c5097
developerHub01 <developerhubytchannel@gmail.com>
2025-01-18 22:58:36 +0600

    header updated

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,12 +1,20 @@
 import React from "react";
-import { SidebarTrigger } from "../sidebar/app-sidebar/app-sidebar-root";
+import { Link } from "react-router";
+import { SidebarTrigger } from "~/components/sidebar/app-sidebar/app-sidebar-root";
+import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
 
 const Header = () => {
   return (
-    <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-14 shadow-lg border-b">
+    <header className="flex justify-between h-16 px-5 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-14 shadow-lg border-b">
       <div className="flex items-center gap-2 px-4">
         <SidebarTrigger className="-ml-1" />
       </div>
+      <Link to="/">
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </Link>
     </header>
   );
 };
