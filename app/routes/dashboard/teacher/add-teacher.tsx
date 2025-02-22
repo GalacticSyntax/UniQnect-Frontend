a4c062cdecbbe79702f1369e0c1474e5cbe88c84
developerHub01 <developerhubytchannel@gmail.com>
2025-02-23 04:57:03 +0600

    feat: implement teacher addition functionality with API integration and loader state

--- app/routes/dashboard/teacher/add-teacher.tsx
+++ app/routes/dashboard/teacher/add-teacher.tsx
@@ -1,5 +1,10 @@
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import { toast } from "sonner";
 import type { AlignType } from "~/components/form/BatManForm";
 import BatManForm from "~/components/form/BatManForm";
+import { axiosClient } from "~/lib/apiClient";
 
 const formSchema = {
   title: {
@@ -99,13 +104,43 @@ const formSchema = {
 };
 
 const AddTeacher = () => {
-  const handleFormSubmit = (formData: Record<string, unknown>) => {
-    console.log(formData);
+  const [loader, setLoader] = useState(false);
+  const navigate = useNavigate();
+
+  const handleFormSubmit = async (formData: Record<string, unknown>) => {
+    setLoader(true);
+
+    try {
+      const response = await axiosClient.post("/user", {
+        ...formData,
+        role: "teacher",
+      });
+
+      const data = await response.data;
+      setLoader(false);
+
+      toast("Teacher added", {
+        description: `${data?.data?.name} teacher added successfully`,
+      });
+
+      navigate("/dashboard/teacher");
+    } catch (error: unknown) {
+      setLoader(false);
+      toast("Error occure", {
+        description: axios.isAxiosError(error)
+          ? error?.response?.data?.message
+          : "Something went wrong",
+      });
+    }
   };
 
   return (
     <section className="w-full h-full grid place-items-center p-5">
-      <BatManForm formSchema={formSchema} onSubmit={handleFormSubmit} className="max-w-3xl" />
+      <BatManForm
+        formSchema={formSchema}
+        onSubmit={handleFormSubmit}
+        className="max-w-3xl"
+      />
     </section>
   );
 };
