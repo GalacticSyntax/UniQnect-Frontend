e6fd046319241e3067093cb8de2a2b0e65316ad6
developerHub01 <developerhubytchannel@gmail.com>
2025-02-24 01:24:25 +0600

    feat: implement login functionality with form handling and authentication; wrap dashboard in PrivateRoute for access control

--- app/routes/auth/login.tsx
+++ app/routes/auth/login.tsx
@@ -1,14 +1,15 @@
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-} from "~/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
 import type { Route } from "./+types/login";
 import { cn } from "~/lib/utils";
 import { Button } from "~/components/ui/button";
 import { Label } from "@radix-ui/react-label";
 import { Input } from "~/components/ui/input";
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import { axiosClient } from "~/lib/apiClient";
+import { toast } from "sonner";
+import axios from "axios";
+import { useAuth } from "~/provider/AuthProvider";
 
 export const meta = ({}: Route.MetaArgs) => {
   return [
@@ -21,6 +22,54 @@ const Login = ({
   className,
   ...props
 }: React.ComponentPropsWithoutRef<"div">) => {
+  const { login } = useAuth(); // Get login function from context
+  // State to hold all form data
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const navigate = useNavigate();
+
+  // Handle input change for all fields
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value, // Dynamically update the corresponding field
+    });
+  };
+
+  // Handle form submission
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    // Handle the login logic here, e.g., call an API
+
+    try {
+      const response = await axiosClient.post("/auth/login", {
+        ...formData,
+        role: "admission-office",
+      });
+
+      const data = await response?.data?.data;
+
+      if (!data) return null;
+
+      login(data.user);
+
+      toast("Loggedin successfully", {
+        description: `Loggedin successfully`,
+      });
+
+      navigate("/dashboard");
+    } catch (error: unknown) {
+      toast("Error occure", {
+        description: axios.isAxiosError(error)
+          ? error?.response?.data?.message
+          : "Something went wrong",
+      });
+    }
+  };
+
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
@@ -28,23 +77,33 @@ const Login = ({
           <CardTitle className="text-xl">Welcome back</CardTitle>
         </CardHeader>
         <CardContent>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="grid gap-6">
               <div className="grid gap-6">
                 <div className="grid gap-2">
                   <Label htmlFor="email">Email</Label>
                   <Input
                     id="email"
+                    name="email" // Use the name attribute for state updates
                     type="email"
                     placeholder="m@example.com"
                     required
+                    value={formData.email} // Sync with state
+                    onChange={handleChange} // Handle change
                   />
                 </div>
                 <div className="grid gap-2">
                   <div className="flex items-center">
                     <Label htmlFor="password">Password</Label>
                   </div>
-                  <Input id="password" type="password" required />
+                  <Input
+                    id="password"
+                    name="password" // Use the name attribute for state updates
+                    type="password"
+                    required
+                    value={formData.password} // Sync with state
+                    onChange={handleChange} // Handle change
+                  />
                 </div>
                 <Button type="submit" className="w-full">
                   Login
--- app/routes/dashboard/dashboard.tsx
+++ app/routes/dashboard/dashboard.tsx
@@ -3,6 +3,7 @@ import StudentsVSCourses from "~/components/charts/StudentsVSCourses";
 import AttendanceDistribution from "~/components/charts/AttendanceDistribution";
 import StudentVsDepartment from "~/components/charts/StudentVsDepartment";
 import TeacherVsDepartment from "~/components/charts/TeacherVsDepartment";
+import PrivateRoute from "~/components/PrivateRoute";
 
 export const meta = ({}: Route.MetaArgs) => {
   return [
@@ -13,12 +14,14 @@ export const meta = ({}: Route.MetaArgs) => {
 
 const DashboardPage = () => {
   return (
-    <section className="p-5 grid md:grid-cols-2 gap-5">
-      <StudentsVSCourses />
-      <AttendanceDistribution />
-      <StudentVsDepartment />
-      <TeacherVsDepartment />
-    </section>
+    <PrivateRoute>
+      <section className="p-5 grid md:grid-cols-2 gap-5">
+        <StudentsVSCourses />
+        <AttendanceDistribution />
+        <StudentVsDepartment />
+        <TeacherVsDepartment />
+      </section>
+    </PrivateRoute>
   );
 };
 
